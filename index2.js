d84c2c955ddb7b76d3ebb90ebc28f230e09192b7
Rich <diverdwn06@outlook.com>
2022-12-13 09:28:50 -0600

    commit week2

---
+++ index2.js
@@ -0,0 +1,14 @@
+var dozenEggs = 4
+if (dozenEggs < 1){
+    console.log("let's buy 4 dozen then" );
+    else if (dozenEggs > 1 && < 2){
+        console.log("let's buy only 3 dozen");
+    }
+    else if (dozenEggs > 2 < 3){
+        console.log("let's only buy 2 dozen");
+    }
+    else {
+        console.log("let's not buy any today");
+
+    }
+}
